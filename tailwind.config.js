1fe6bb270de86d23a23250ae49fe40ae71f3a020
BryanCambar30 <49474942+BryanCambar30@users.noreply.github.com>
2024-06-20 00:56:39 -0600

    'guides'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}', // Asegúrate de incluir las rutas correctas
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
